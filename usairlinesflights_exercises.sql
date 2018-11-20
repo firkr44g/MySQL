c58a28de066bd3781ce7eefe7eaa2c9e35027c85
firkr44g <gverareina@gmail.com>
2018-11-20 09:41:12 +0100

    Last update ( ex. 4 & 6 corrections).

--- usairlinesflights_exercises.sql
+++ usairlinesflights_exercises.sql
@@ -20,12 +20,15 @@ order by 'Aeroport Origen', 'Any', 'Mes' asc;
 /* 4. Retard promig d’arribada dels vols, per mesos i segons l’aeroport origen (mateixa consulta que abans
  i amb el mateix ordre). Però a més, ara volen que en comptes del codi de l’aeroport es mostri el nom de la ciutat. */
 
-SELECT airport as 'Aeroport Origen', colYear as 'Any', colMonth as 'Mes',  avg(ArrDelay) as 'Retard Arribada'
-FROM usairlineflights2.usairports 
-INNER JOIN usairlineflights2.flights 
-ON usairports.IATA = flights.flightID 
-group by 'Aeroport Origen', 'Any', 'Mes'
-order by 'Aeroport Origen', 'Any', 'Mes' asc;
+SELECT CONCAT(City,', ',
+	   colYear,', ',
+       RIGHT(CONCAT('0',colMonth),2),', ',
+       IF(AVG(ArrDelay)>0,'delay','on time')) AS DelayByCity
+FROM Flights
+LEFT JOIN USAirports
+ON Flights.Origin=USAirports.IATA
+GROUP BY Origin, colYear, colMonth
+ORDER BY Origin, colYear, colMonth;
 
 /* 5.  Les companyies amb més vols cancelats. A més, han d’estar ordenades de forma que les companyies amb més cancelacions
  apareguin les primeres.  */
@@ -40,11 +43,12 @@ order by 'Vols Cancel·lats' desc;
 
 /* 6. L’identificador dels 10 avions que més distància han recorregut fent vols. */
 
-SELECT TailNum as 'Avió', sum(distance) as 'Distància' 
-from usairlineflights2.flights 
-group by TailNum 
-order by Distància 
-DESC LIMIT 10;
+SELECT TailNum,SUM(Distance) AS TotalMiles
+FROM Flights
+WHERE NOT TailNum LIKE 'NA'
+GROUP BY TailNum
+ORDER BY SUM(Distance) DESC
+LIMIT 10;
 
 /* 7. Companyies amb el seu retard promig només d’aquelles les quals els seus vols arriben
  al seu destí amb un retràs promig major de 10 minuts.  */
